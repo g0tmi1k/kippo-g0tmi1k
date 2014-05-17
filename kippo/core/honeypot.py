e9121b3a4151318494c4f3ec93a299d88053d051
g0tmi1k <have.you.g0tmi1k@gmail.com>
2014-05-17 13:19:24 +0100

    Update "shell prompt" (Debian feel) Looks more of the default Debian/Ubuntu (which is the base OS). Check comments for CentOS verison.

--- kippo/core/honeypot.py
+++ kippo/core/honeypot.py
@@ -136,10 +136,17 @@ class HoneyPotShell(object):
         self.runCommand()
 
     def showPrompt(self):
+        # Example: nas3:~#
+        #prompt = '%s:%%(path)s' % self.honeypot.hostname
+        # Example: root@nas3:~#  (More of a "Ubuntu" feel)
+        prompt = '%s@%s:%%(path)s' % (self.honeypot.user.username, self.honeypot.hostname,)
+        # Example: [root@localhost ~]#  (More of a "CentOS" feel)
+        #prompt = '[%s@%s %%(currentfolder)s]' % (self.honeypot.user.username, self.honeypot.hostname,)
+
         if not self.honeypot.user.uid:
-            prompt = '%s:%%(path)s# ' % self.honeypot.hostname
+            prompt += '# ' # "Root" user
         else:
-            prompt = '%s:%%(path)s$ ' % self.honeypot.hostname
+            prompt += '$ ' # "Non-Root" user
 
         path = self.honeypot.cwd
         homelen = len(self.honeypot.user.home)
@@ -149,7 +156,8 @@ class HoneyPotShell(object):
                 path[:(homelen+1)] == self.honeypot.user.home + '/':
             path = '~' + path[homelen:]
 
-        attrs = {'path': path}
+        #currentfolder=path.split(/)[-1]
+        attrs = {'path': path} #,'currentfolder': currentfolder
         self.honeypot.terminal.write(prompt % attrs)
 
     def ctrl_c(self):
